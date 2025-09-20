c0654f595e758bb1a779a2b07690fead90755abe
vicp-cloud <paesevictor@gmail.com>
2025-09-20 19:31:03 -0300

    Create test_exemplo.py

---
+++ tests/test_exemplo.py
@@ -0,0 +1,16 @@
+# tests/test_exemplo.py
+
+def test_soma():
+    assert 1 + 1 == 2
+
+def test_subtracao():
+    assert 5 - 3 == 2
+
+def test_multiplicacao():
+    assert 3 * 3 == 9
+
+def test_divisao():
+    assert 8 / 2 == 4
+
+def test_string():
+    assert "github".upper() == "GITHUB"
